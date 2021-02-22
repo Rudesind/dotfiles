950feba48fe949b86470f9ef27ec6484c72f74d4
Rudesind <rudesind76@gmail.com>
2021-02-21 18:13:24 -0800

    Update powershell profile.

--- .config/powershell/profile.ps1
+++ .config/powershell/profile.ps1
@@ -1,8 +1,3 @@
-<#
-    Profile: MyProfile.ps1
-    Author : Rudesind <rudesind76@gmail.com>
-#>
-
 # Set the location of the terminal to root
 #
 set-location $home > $null
@@ -15,28 +10,10 @@ set-location $home > $null
 #
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
+    Import-Module "$ChocolateyProfile"
 }
-
-# Drive for ServiceNow Development
-#
-New-PSDrive -Name sndev -PSProvider FileSystem -Root "~/urmstores.onmicrosoft.com\ServiceNow - Documents\Development" > $null
-
-# New-PSDrive -Name git -PSProvider FileSystem -Root "C:\Users\zach2\git" > $null
-# New-PSDrive -Name CMDocs -PSProvider FileSystem -Root "C:\Users\zach2\urmstores.onmicrosoft.com\Configuration Management - Documents" > $null
-
-<#
-# Create working directory to config management docs
-#
-New-PSDrive -Name CMDocs -PSProvider FileSystem -Root "C:\Users\zach2\urmstores.onmicrosoft.com\Configuration Management - Documents" 
-
-New-PSDrive -Name CMImages -PSProvider FileSystem -Root "C:\Users\zach2\urmstores.onmicrosoft.com\Enterprise Imaging - Documents"
-
-New-PSDrive -Name CMPatching -PSProvider FileSystem -Root "C:\Users\zach2\urmstores.onmicrosoft.com\Enterprise Patching - Documents"
-#>
-
 
 # Clear the PowerShell starting information
 #
-Clear-Host
+#Clear-Host
 Invoke-Expression (&starship init powershell)

3a2e86dbf497f8b509098648a27bae6b385bf20b
Rudesind <rudesind76@gmail.com>
2024-01-18 21:56:03 -0800

    Add telescope startup configuration.

--- AppData/Local/nvim/init.lua
+++ AppData/Local/nvim/init.lua
@@ -3,3 +3,4 @@ require("base") -- General Terminal Settings
 require("bootstrap") -- For plugin manager
 require("lazy").setup("plugins") -- Load Plugin Manager
 require("colorscheme") -- For colors
+require("telescope").setup("telescope_config") -- Load the Telescope plugin
